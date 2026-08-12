b6223377eb3bfbe93716dcac150bbae36270940b
test243281-ui <test243281@gmail.com>
2026-08-12 11:57:09 +0600

    add initial implementation of item class and main function

--- lab 5/greaddy.cpp
+++ lab 5/greaddy.cpp
@@ -1,1 +1,23 @@
-#include<
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Item
+{
+public:
+    double weight;
+    double value;
+};
+
+int main()
+{
+    vector<Item> items = {
+        {4.0, 20.0},
+        {3.0, 9.0},
+        {2.0, 12.0},
+        {1.0, 7.0}
+    };
+    int cap=5;
+    cout<< endl ;
+    return 0;
+}
